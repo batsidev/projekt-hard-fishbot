251fe4228de93f95f5953348fb5d49ad697ba8eb
batsidev <118818623+batsidev@users.noreply.github.com>
2026-05-17 03:32:44 +0200

    Update fisher.py

--- fisher.py
+++ fisher.py
@@ -294,7 +294,7 @@ def check_for_image():
 
         pull_hook(best_match['name'])
         time.sleep(4)
-        _fishing()
+        prepare_for_fishing()
         return False
 
     detection_attempts += 1
